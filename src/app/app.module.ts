06fdedb14a2c3faab2dbdc17027e19dafc4e6612
Jimmy Ranger <jimmy.b.ranger@gmail.com>
2017-04-28 13:16:24 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarModule } from 'angular-calendar';
+
+import { DemoUtilsModule } from './demo-utils.module';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    CommonModule,
     FormsModule,
-    HttpModule
+    NgbModalModule.forRoot(),
+    DemoUtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
